178c9db39f2d86411bc52aae9dba25e799df53e0
Francegala <francescogalassimobile@gmail.com>
2020-04-04 21:19:55 +0100

    template done

--- index.php
+++ index.php
@@ -1,15 +1,64 @@
 <html>
- <head>
-  <title>PHP Test</title>
- </head>
- <body>
- <?php 
-	$email = $_GET['email'];
-	$token= $_GET['token'];
- echo $email; 
- echo'<br/>';
-  echo $token; 
+<head>
+<link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.css" rel="stylesheet">
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
 
- ?> 
- </body>
+<style>
+.form-gap {
+    padding-top: 70px;
+}
+
+</style>
+
+</head>
+<body>
+ <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.5.0/css/font-awesome.min.css">
+ <div class="form-gap"></div>
+<div class="container">
+	<div class="row">
+		<div class="col-md-4 col-md-offset-4">
+            <div class="panel panel-default">
+              <div class="panel-body">
+                <div class="text-center">
+                  <h3><i class="fa fa-lock fa-4x"></i></h3>
+                  <h2 class="text-center">Forgot Password?</h2>
+                  <p>You can reset your password here.</p>
+                  <div class="panel-body">
+                    <form id="register-form" action="index.php" role="form" autocomplete="off" class="form" method="post"><!--Form-->
+    
+                      <div class="form-group">
+                        <div class="input-group">
+                          <span class="input-group-addon"><i class="fa fa-unlock fa-lg"></i></span>
+                          <input id="pass1" name="pass1" placeholder="New Password" class="form-control"  type="password"><!--pass1-->
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <div class="input-group">
+                          <span class="input-group-addon"><i class="fa fa-unlock fa-lg"></i></span>
+                          <input id="pass2" name="pass2" placeholder="Confirm new password" class="form-control"  type="password"><!--pass1-->
+                        </div>
+                      </div>
+                      <div class="form-group">
+                        <input name="submit" class="btn btn-lg btn-primary btn-block" value="Reset Password" type="submit"><!--submit-->
+                      </div>
+                      
+                      <input type="hidden" class="hide" name="token" id="token" value=""> 
+                    </form>
+    
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+	</div>
+    <php
+
+        echo ($_POST['pass1']);
+    
+?>
+</div>
+</body>
 </html>
