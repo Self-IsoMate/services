67722fea04a9f9fca7b78f192eaa7ef6bbec6ac1
Francegala <francescogalassimobile@gmail.com>
2020-04-04 19:00:48 +0100

    initializing repo

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+ <head>
+  <title>PHP Test</title>
+ </head>
+ <body>
+ <?php 
+	$email = $_GET['email'];
+	$token= $_GET['token'];
+ echo $email; 
+ echo'<br/>';
+  echo $token; 
+
+ ?> 
+ </body>
+</html>
